2a9b367a3516327727e7e0a71b259e6a79c37ee0
JTippetts <vertexnormal@gmail.com>
2021-12-29 14:54:56 -0700

    Implement sun settings for fog color, sun color, and sun location.

--- main.cpp
+++ main.cpp
@@ -23,11 +23,99 @@
 #include <Urho3D/Graphics/Terrain.h>
 #include <Urho3D/Graphics/Light.h>
 #include <Urho3D/Graphics/Zone.h>
+#include <Urho3D/Math/Random.h>
 #include <cmath>
-
-
 // This is probably always OK.
 using namespace Urho3D;
+
+
+
+struct EnvironmentPreset
+{
+	float Br_{0}, Bm_{0}, g_{0}, cirrus_{0}, cumulus_{0}, cumulusbrightness_{0};
+	
+	EnvironmentPreset Lerp(const EnvironmentPreset &rhs, float t)
+	{
+		EnvironmentPreset p;
+		p.Br_=Br_ + t * (rhs.Br_ - Br_);
+		p.Bm_=Bm_ + t * (rhs.Bm_ - Bm_);
+		p.g_=g_ + t * (rhs.g_ - g_);
+		p.cirrus_=cirrus_ + t * (rhs.cirrus_ - cirrus_);
+		p.cumulus_=cumulus_ + t * (rhs.cumulus_ - cumulus_);
+		p.cumulusbrightness_=cumulusbrightness_ + t * (rhs.cumulusbrightness_ - cumulusbrightness_);
+		
+		return p;
+	}
+};
+
+struct EnvironmentPresetTemplate
+{
+	float Br_{0}, Bm_{0}, g_{0}, cumulusbrightness_{0};
+	float cirruslow_{0}, cirrushigh_{0}, cumuluslow_{0}, cumulushigh_{0};
+	
+	EnvironmentPreset Randomize()
+	{
+		EnvironmentPreset p;
+		p.Br_=Br_;
+		p.Bm_=Bm_;
+		p.g_=g_;
+		p.cumulusbrightness_=cumulusbrightness_;
+		
+		p.cirrus_ = cirruslow_ + RandStandardNormal() * (cirrushigh_ - cirruslow_);
+		p.cumulus_ = cumuluslow_ + RandStandardNormal() * (cirrushigh_ - cirruslow_);
+		return p;
+	}
+};
+
+struct SunSettings
+{
+	Vector3 sunpos_{0,0,0};
+	Color suncolor_{0,0,0};
+	Color fogcolor_{0,0,0};
+};
+
+Color CalculateSkyboxColor(float timeofday, const Vector3 &pos, const EnvironmentPreset &env)
+{
+	const Vector3 nitrogen(0.650, 0.570, 0.475);
+	auto vecpow=[](const Vector3 &vec, float p)->Vector3 {return Vector3(std::pow(vec.x_, p), std::pow(vec.y_, p), std::pow(vec.z_, p));};
+	auto vecdiv=[](float c, const Vector3 &vec)->Vector3 {return Vector3(c/vec.x_, c/vec.y_, c/vec.z_);};
+	auto vecexp=[](const Vector3 &vec)->Vector3 {return Vector3(std::exp(vec.x_), std::exp(vec.y_), std::exp(vec.z_));};
+	
+	Vector3 fsun(0.0, std::sin(timeofday * 0.2617993875), std::cos(timeofday * 0.2617993875));
+	float Br=env.Br_;
+	float Bm=env.Bm_;
+	float g=env.g_;
+	Vector3 Kr = vecdiv(Br, vecpow(nitrogen, 4.f));
+	Vector3 Km = vecdiv(Bm, vecpow(nitrogen, 0.84f));
+	
+	float mu = pos.DotProduct(fsun);
+	float rayleigh = 3.f / (8.0f * 3.14f) * (1.0f + mu * mu);
+	Vector3 mie = (Kr + Km * (1.0f - g * g) / (2.0f + g * g) / pow(1.0f + g * g - 2.0f * g * mu, 1.5f)) / (Br + Bm);
+	Vector3 day_extinction=vecexp((Kr / Br)*-exp(-((pos.y_ + fsun.y_ * 4.0f) * (exp(-pos.y_ * 16.0f) + 0.1f) / 80.0f) / Br) * (exp(-pos.y_ * 16.0f) + 0.1f)) * exp(-pos.y_ * exp(-pos.y_ * 8.0f ) * 4.0f) * exp(-pos.y_ * 2.0f) * 4.0f;
+	float v=1.0f - std::exp(fsun.y_);
+	Vector3 night_extinction=Vector3(v,v,v)*0.2f;
+	Vector3 extinction=day_extinction.Lerp(night_extinction, -fsun.y_ * 0.2f + 0.5f);
+	Vector3 c=mie*extinction*rayleigh;
+	return Color(c.x_, c.y_, c.z_);
+}
+
+SunSettings CalculateSunSettings(float timeofday, float abovehorizon, const EnvironmentPreset &env)
+{
+	SunSettings sun;
+	sun.sunpos_ = Vector3(0.0, std::sin(timeofday * 0.2617993875), std::cos(timeofday * 0.2617993875));
+	Vector3 pos(1, abovehorizon, 0);
+	pos.Normalize();
+	
+	if(pos.y_ < 0.f) pos.y_ = 0.f;
+	
+	sun.fogcolor_=CalculateSkyboxColor(timeofday, pos, env);
+	sun.suncolor_=CalculateSkyboxColor(timeofday, sun.sunpos_, env);
+	sun.suncolor_.r_=std::min(sun.suncolor_.r_, 1.f);
+	sun.suncolor_.g_=std::min(sun.suncolor_.g_, 1.f);
+	sun.suncolor_.b_=std::min(sun.suncolor_.b_, 1.f);
+	
+	return sun;
+}
 
 class AwesomeGameApplication : public Application
 {
@@ -89,16 +177,16 @@ public:
 		zone_->SetFogEnd(750.0f);
 
 		// Create a directional light to the world. Enable cascaded shadows on it
-		Node* lightNode = scene_->CreateChild("DirectionalLight");
-		lightNode->SetDirection(Vector3(0.6f, -1.0f, 0.8f));
-		auto* light = lightNode->CreateComponent<Light>();
-		light->SetLightType(LIGHT_DIRECTIONAL);
-		light->SetCastShadows(true);
-		light->SetShadowBias(BiasParameters(0.00025f, 0.5f));
-		light->SetShadowCascade(CascadeParameters(10.0f, 50.0f, 200.0f, 0.0f, 0.8f));
-		light->SetSpecularIntensity(0.5f);
+		lightNode_ = scene_->CreateChild("DirectionalLight");
+		lightNode_->SetDirection(Vector3(0.6f, -1.0f, 0.8f));
+		light_ = lightNode_->CreateComponent<Light>();
+		light_->SetLightType(LIGHT_DIRECTIONAL);
+		light_->SetCastShadows(true);
+		light_->SetShadowBias(BiasParameters(0.00025f, 0.5f));
+		light_->SetShadowCascade(CascadeParameters(10.0f, 50.0f, 200.0f, 0.0f, 0.8f));
+		light_->SetSpecularIntensity(0.5f);
 		// Apply slightly overbright lighting to match the skybox
-		light->SetColor(Color(1.2f, 1.2f, 1.2f));
+		light_->SetColor(Color(1.2f, 1.2f, 1.2f));
 		Node* terrainNode = scene_->CreateChild("Terrain");
 		terrainNode->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 		auto* terrain = terrainNode->CreateComponent<Terrain>();
@@ -156,7 +244,7 @@ public:
 		if(s)
 		{
 			float v=(s->GetValue()/100.0f);
-			v=0.0001 + v * (0.009 - 0.0001);
+			v=0.0001 + v * (0.09 - 0.0001);
 			skyboxmaterial_->SetShaderParameter("Bm", Variant(v));
 			Text *t=dynamic_cast<Text *>(element_->GetChild("BmValue", true));
 			if(t)
@@ -265,6 +353,18 @@ public:
 		{
 			input->SetMouseVisible(true);
 		}
+		
+		EnvironmentPreset p;
+		p.Br_=Br;
+		p.Bm_=Bm;
+		p.g_=g;
+	
+		SunSettings sun=CalculateSunSettings(timeofday, 0.0f, p);
+		zone_->SetFogColor(sun.fogcolor_);
+		zone_->SetAmbientColor(Color(sun.fogcolor_.r_*0.2f, sun.fogcolor_.g_*0.2f, sun.fogcolor_.b_*0.2f));
+		lightNode_->SetDirection(-sun.sunpos_);
+		light_->SetColor(sun.suncolor_);
+		
 		MoveCamera(timeStep);
 	}
 	void MoveCamera(float timeStep)
@@ -307,13 +407,14 @@ public:
 	
 	protected:
 	SharedPtr<Scene> scene_;
-	Node *cameraNode_;
+	Node *cameraNode_, *lightNode_;
 	Camera *camera_;
 	float yaw_{0}, pitch_{-20.7f};
 	float time_{0};
 	Material *skyboxmaterial_{nullptr};
 	SharedPtr<UIElement> element_;
 	Zone *zone_{nullptr};
+	Light *light_{nullptr};
 	
 	void HandleUpdate(StringHash eventType, VariantMap &eventData)
 	{
